7c06866867cf04486bb6780ddb548410d610642d
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-03-10 16:07:26 +0530

    Trainers section added

---
+++ src/components/TrainerCard.jsx
@@ -0,0 +1,19 @@
+import "./TrainerCard.css"
+import { FaInstagram, FaLinkedin  } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+export default function TrainerCard({trainer}) {
+    return (
+        <div className="card col-12 col-sm-5 col-md-3 mb-3">
+            <img src={trainer.img} className="card-img-top p-1" alt="Trainer"/>
+            <div className="card-body">
+              <h4 className="card-title text-center">{trainer.name}</h4>
+              <div className="trainer-socials d-flex justify-content-around pt-1">
+                <a href={trainer.ig || "#"} target="_blank"><FaInstagram size={"1.25em"} className="social-icons" /></a>
+                <a href={trainer.twitter || "#"} target="_blank"><FaXTwitter size={"1.25em"} className="social-icons" /></a>
+                <a href={trainer.linkedIn || "#"} target="_blank"><FaLinkedin size={"1.25em"} className="social-icons" /></a>
+              </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/TrainerSection.jsx
@@ -0,0 +1,22 @@
+import TrainerCard from "./TrainerCard"
+import "./TrainerSection.css"
+
+export default function TrainerSection() {
+
+    const trainers = [
+        {name: "Amar Singh", img: "images/trainer1.jpg",},
+        {name: "Akbar Ali", img: "images/trainer2.jpg",},
+        {name: "Anthony D'Souza", img: "images/trainer3.jpg",},
+    ]
+
+    return (
+        <section className="TrainerSection" id="trainers">
+            <h1 className="text-center p-4">Our Trainers</h1>
+            <div className="trainer-cards row d-flex flex-wrap justify-content-between px-4 px-md-5 w-100">
+                {trainers.map((t, idx)=>(
+                    <TrainerCard trainer={t} key={idx} />
+                ))}
+            </div>
+        </section>
+    )
+}
